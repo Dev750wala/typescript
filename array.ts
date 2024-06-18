329ee3fd6a57a758210eb49a78e74659def2228f
Dev Sadisatsowala <117472132+Dev750wala@users.noreply.github.com>
2024-06-18 21:08:50 +0530

    first commit

---
+++ array.ts
@@ -0,0 +1,21 @@
+const superHeros: string[] = []
+// const heroPower: number[] = []
+const heroPower: Array<number> = []
+
+type user = {
+    name: string
+    isActive: boolean
+}
+
+const allUsers: user[] = []
+
+const MLModels: number[][] = [
+    [255, 255, 255],
+    []
+]
+
+
+superHeros.push("spiderman")
+heroPower.push(2)
+
+allUsers.push({name: "", isActive: true})
---
+++ intro.js
@@ -0,0 +1,7 @@
+var person = [
+    {
+        firstName: "Dev",
+        lastName: "750wala",
+    }
+];
+console.log(person);
